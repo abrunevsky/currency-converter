f1a0da5f7baa6a43071d7a2fc21dbe8447cafcdf
Alexander Brunevsky <a.brunevsky@planner5d.com>
2024-05-23 12:27:30 +0300

    CS fixes

--- src/Service/ExchangeSource/CbrRu/CbrHttpClientCacheable.php
+++ src/Service/ExchangeSource/CbrRu/CbrHttpClientCacheable.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Service\ExchangeSource\CbrRu;
 
-use Symfony\Component\Cache\CacheItem;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class CbrHttpClientCacheable implements CbrHttpClientInterface
 {
@@ -20,9 +20,10 @@ class CbrHttpClientCacheable implements CbrHttpClientInterface
 
     public function loadXml(): \SimpleXMLElement
     {
-        $serialisedXml = $this->cache->get(self::CAHCE_KEY, function(CacheItem $item) {
+        $serialisedXml = $this->cache->get(self::CAHCE_KEY, function (ItemInterface $item) {
             $item->expiresAfter($this->ttl);
-            return $this->client->loadXml()->asXML();
+
+            return (string) $this->client->loadXml()->asXML();
         });
 
         return new \SimpleXMLElement($serialisedXml);
--- src/Service/ExchangeSource/CbrRu/CbrHttpClientInterface.php
+++ src/Service/ExchangeSource/CbrRu/CbrHttpClientInterface.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace App\Service\ExchangeSource\CbrRu;
 
-use GuzzleHttp\Psr7\Request;
-use Psr\Http\Client\ClientInterface;
-
 interface CbrHttpClientInterface
 {
     public function loadXml(): \SimpleXMLElement;
